1e9576834915fb8cd0535db02704339d3b62fb67
kosakik <katsuo.karaage@gmail.com>
2021-08-07 18:20:12 +0900

    チャットルーム作成機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,4 +2,23 @@ class RoomsController < ApplicationController
   def new
     @room = Room.new
   end
+
+  def create
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:name, user_ids: [])
+    #user_ids: []は、配列としてきているデータを許可する時の記述
+    #permitは、データの使用許可を意味する
+    #:roomはモデル名
+  end
+
 end
